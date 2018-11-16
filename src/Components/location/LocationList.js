07df0859af59db8a70b8bcfbc4ccd07c41b35e8e
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2018-11-16 14:08:56 -0600

    finished product

---
+++ src/Components/location/LocationList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class LocationList extends Component {
+  render(){
+    return(
+    <div>
+      <h1> locations</h1>
+      <section> Nashville North</section>
+      <section> Nashville South</section>
+    </div>
+    )
+  }
+}
